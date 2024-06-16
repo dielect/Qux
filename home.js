42200eb0d4cd75987eaa9101478a9d686df7b1a5
dielect <dielectric.army@gmail.com>
2024-06-16 19:34:48 +0800

    Create home.js

---
+++ home.js
@@ -0,0 +1,217 @@
+// customResponse.js
+let body = {
+  "code" : 0,
+  "data" : {
+    "tab_visible" : {
+      "collect" : true,
+      "note" : true,
+      "seed" : true,
+      "like" : true
+    },
+    "profile_editable" : {
+      "red_id" : true,
+      "nickname" : true,
+      "image" : true,
+      "desc" : true
+    },
+    "hey_info" : {
+      "display" : true,
+      "deep_link" : "xhsdiscover://hey_home_feed"
+    },
+    "collected_poi_num" : 0,
+    "liked" : 10000,
+    "app_first_time" : "1715073291",
+    "result" : {
+      "success" : true,
+      "message" : "success",
+      "code" : 0
+    },
+    "collected" : 0,
+    "follows" : 0,
+    "collected_filters_num" : 0,
+    "is_default_avatar" : true,
+    "recommend_info" : "",
+    "red_official_verify_base_info" : "",
+    "default_collection_tab" : "",
+    "red_club_info" : {
+      "red_club_url" : "https://www.xiaohongshu.com/store/mc/landing",
+      "red_club_level" : 0,
+      "redclubscore" : 0,
+      "red_club" : false
+    },
+    "share_link" : "https://www.xiaohongshu.com/user/profile/6639f10a000000000b0304fd",
+    "banner_info" : {
+      "image" : "http://sns-avatar-qc.xhscdn.com/user_banner/default/h30-b100.png?imageView2/2/w/540/format/jpg",
+      "bg_color" : "ebafc1",
+      "allow_edit" : true
+    },
+    "easemob_password" : "9cfc45499869dd4b5624db88f77188a5",
+    "share_info" : {
+      "title" : "小红书官方助手",
+      "content" : "Click here to fill in the profile",
+      "avatar" : "https://sns-avatar-qc.xhscdn.com/avatar/645b7f3c86578b8c6ab3b068.jpg?imageView2/2/w/540/format/webp"
+    },
+    "identity_deeplink" : "xhsdiscover://rn/app-settings/official/certification/details?type=1&user_id=6639f10a000000000b0304fd&is_mcn=false",
+    "red_official_verify_type" : 0,
+    "imageb" : "https://sns-avatar-qc.xhscdn.com/avatar/645b7f3c86578b8c6ab3b068.jpg?imageView2/2/w/540/format/webp",
+    "note_tab_info" : {
+      "type" : "NOTE_DEFAULT",
+      "sub_tab" : [
+        {
+          "type" : "NONE",
+          "num" : 0,
+          "show" : false
+        }
+      ]
+    },
+    "order_list_link" : "https://www.xiaohongshu.com/order/list?sid=session.1718514562293581790365",
+    "collected_product_num" : 0,
+    "collected_notes_num" : 0,
+    "mom_infant" : {
+      "type" : 0
+    },
+    "type" : "phone",
+    "location_jump" : false,
+    "desc" : "Click here to fill in the profile",
+    "location" : "完善你的位置信息",
+    "skin_qa_url" : "https://www.xiaohongshu.com/activity/skin_selection",
+    "is_birthday" : false,
+    "sessionid" : "session.1718514562293581790365",
+    "is_edition_in_review" : false,
+    "downgrade" : false,
+    "is_recommend_level_illegal" : false,
+    "seller_info" : {
+      "tab_code_names" : [
+
+      ],
+      "is_tab_goods_first" : true,
+      "tab_goods_api_version" : 1
+    },
+    "ndiscovery" : 0,
+    "feedback_account_appeal_url" : "https://fe.xiaohongshu.com/apps/risk-h5/user-check?fullscreen=true&source=personal_homepage",
+    "userid" : "6639f10a000000000b0304fd",
+    "collected_movie_num" : 0,
+    "identity_label_migrated" : false,
+    "collected_brand_num" : 0,
+    "avatar_pendant" : {
+      "current_user_pendant" : false,
+      "schema" : "https://www.xiaohongshu.com/apps/redcity?fullscreen=true&background_color=D1BFA3&enableObserveKeyboardChange=true&disableDragBack=yes&disableBackForwardNavigationGestures=true&decorationId=&source=profile_photo_obtain_decoration",
+      "current_user_pet" : false
+    },
+    "bind_phone" : true,
+    "session" : "1718514562293581790365",
+    "red_id" : "95296066447",
+    "tags" : [
+
+    ],
+    "desc_pre_edit" : {
+      "allow_edit" : true
+    },
+    "community_rule_url" : "https://www.xiaohongshu.com/user/community-rule",
+    "register_time" : "1715073291",
+    "red_official_verified" : false,
+    "note_num_stat" : {
+      "liked" : 9999,
+      "posted" : 0,
+      "collected" : 9999
+    },
+    "collected_book_num" : 0,
+    "need_verify_id" : false,
+    "recommend_info_icon" : "",
+    "red_official_verify_content" : "",
+    "user_token" : "9cfc45499869dd4b5624db88f77188a5",
+    "level" : {
+      "number" : 0,
+      "image_link" : "https://www.xiaohongshu.com/user/growth",
+      "level_name" : "",
+      "title" : "",
+      "image" : "",
+      "link" : "http://www.xiaohongshu.com/user/growth",
+      "desc" : "一只小红薯的成长，从这里开始",
+      "icon" : "http://s4.xiaohongshu.com/static/huati/c8aca79a3d60e48aa4e410aef6ab15e7.png"
+    },
+    "desc_at_users" : [
+
+    ],
+    "nickname" : "小红薯663A5E56",
+    "interactions" : [
+      {
+        "is_private" : false,
+        "count" : 0,
+        "type" : "follows",
+        "name" : "Following",
+        "toast" : ""
+      },
+      {
+        "is_private" : false,
+        "toast" : "",
+        "type" : "fans",
+        "name" : "Followers",
+        "count" : 0
+      },
+      {
+        "is_private" : false,
+        "toast" : "",
+        "type" : "interaction",
+        "name" : "Likes & Collects",
+        "count" : 0
+      }
+    ],
+    "gender" : 2,
+    "nboards" : 0,
+    "secure_session" : "X38618session.1718514562293581790365",
+    "red_id_can_edit" : true,
+    "fans" : 0,
+    "tab_public" : {
+      "collection" : true,
+      "collection_board" : true,
+      "seed" : true,
+      "collection_note" : true
+    },
+    "collected_tags_num" : 0,
+    "images" : "https://sns-avatar-qc.xhscdn.com/avatar/645b7f3c86578b8c6ab3b068.jpg?imageView2/2/w/360/format/webp",
+    "notice_bar" : {
+      "content" : "Change background",
+      "type" : 3
+    },
+    "share_info_v2" : {
+      "title" : "分享@小红薯663A5E56的个人主页",
+      "content" : "Click here..."
+    },
+    "zhong_tong_bar_info" : {
+      "conversions" : [
+        {
+          "id" : "22",
+          "icon" : "https://picasso-static.xiaohongshu.com/fe-platform/3744f2ed8255fab18db40c9f68cdd1f8a7347890.png",
+          "name" : "购物车",
+          "sub_title" : "查看推荐好物",
+          "link" : "xhsdiscover://rn/lancer-slim/user/shopping_cart?source=profile_page&pre_page=profile_page"
+        },
+        {
+          "id" : "20",
+          "icon" : "https://picasso-static.xiaohongshu.com/fe-platform/07e7def8e60722787ecca5f8516629e0cd152dc8.png",
+          "name" : "创作灵感",
+          "sub_title" : "学创作找灵感",
+          "link" : "xhsdiscover://rn/creator-center/inspiration?source=profile"
+        },
+        {
+          "id" : "21",
+          "icon" : "https://picasso-static.xiaohongshu.com/fe-platform/da6f7a6ddea57698dbb7c1691421b6ebce65a606.png",
+          "name" : "浏览记录",
+          "sub_title" : "看过的笔记",
+          "link" : "xhsdiscover://browsinghistory"
+        }
+      ]
+    }
+  },
+  "success" : true,
+  "msg" : "成功"
+};
+
+$done({
+  status: 200,
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(body)
+});
